115993bfea533dd2a2451a4c19958e4cbf4abd8a
fabiCas-D-04 <fcd040206@gmail.com>
2025-09-25 20:46:54 -0400

    1.-primero lo que hice fue hacer los arreglos en la conexión , le puse mi dirección y luego arregle las palabras mal escritas que puso.
    2.-luego lo que hice fue crear el model "RECIPE" donde agregue todas sus características como el ID, Title,description, etc.
    3.-luego de eso lo que hice fue crear con scaffolding todos los controllers para Categories, Ingredients, Recipes.
    Después
    4.- luego después de creas los scaffolds con los curds y demás trate de hacer el update-database pero no me dejaba porque tenia un error y pedia que haga una migración antes, asi que con el Add-migration examen lo hice y recién me dejo hacer el update-database.
    5.- luego verifique y modifique el layout para poder tener las ventanas para poder crear las recetas, categorías e ingredientes.
    6.- como no se me guardaban me fije en los controllers y tuve que aumentar un "!" para poder forzarlo a que funcione y se guarden los datos cuando creaba y demás.

---
+++ examenCompleto/PrimerParcial/Migrations/20250926001330_examen.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrimerParcial.Migrations
+{
+    /// <inheritdoc />
+    public partial class examen : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Recipes",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Recipes",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
---
+++ examenCompleto/PrimerParcial/Models/Recipe.cs
@@ -0,0 +1,18 @@
+﻿namespace PrimerParcial.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int PreparationTimeMinutes { get; set; }
+        public int Servings { get; set; }
+        public string Instructions { get; set; }
+        public DateTime DateCreated { get; set; }
+        public int CategoryId { get; set; } 
+        public Category Category { get; set; }
+
+        
+        public ICollection<Ingredient> Ingredients { get; set; }
+    }
+}
